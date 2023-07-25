e4d974bdcce50c9fb624ff02d35c51dce1a5fd8f
Saidumar Khamdamov <107342368+umarkhodja@users.noreply.github.com>
2023-07-25 22:53:09 +0600

    content done

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Global World")
+                .foregroundColor(Color.blue)
+            Image(systemName: "globe.europe.africa")
+                
         }
-        .padding()
     }
 }
 
