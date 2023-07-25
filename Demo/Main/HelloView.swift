ae2e2f3408e5101166bed7f91b0613a5aa04050f
javlonbekdev <javlonbekdev@gmail.com>
2023-07-25 21:42:55 +0500

    hello added

---
+++ Demo/Main/HelloView.swift
@@ -0,0 +1,14 @@
+//
+//  HelloView.swift
+//  Demo
+//
+//  Created by Javlonbek Dev on 25/07/23.
+//
+
+import Foundation
+
+class Hello {
+    func printHello() {
+        print("hello")
+    }
+}
