577631e9aa3efe98ad5565ff78501b9e1a120763
Saidumar Khamdamov <107342368+umarkhodja@users.noreply.github.com>
2023-07-25 22:38:21 +0600

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Saidumar Khamdamov on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
